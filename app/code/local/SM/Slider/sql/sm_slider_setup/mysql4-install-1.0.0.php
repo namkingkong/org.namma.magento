31ab5aaf296c05c7e0fd61c81e3855c0fa650c58
Nam Mai Anh <namkingkong@hotmail.com>
2014-09-09 16:35:38 +0700

    Update module: SM_Slider

--- app/code/local/SM/Slider/sql/sm_slider_setup/mysql4-install-1.0.0.php
+++ app/code/local/SM/Slider/sql/sm_slider_setup/mysql4-install-1.0.0.php
@@ -5,4 +5,33 @@
  * Date: 9/9/14
  * Time: 3:38 PM
  */
-die ('asdsad');
+$this->startSetup();
+
+$sliderTableName = $this->getTable('sm_slider/slider');
+$imageTableName = $this->getTable('sm_slider/image');
+
+$this->run("
+	DROP TABLE IF EXISTS `{$imageTableName}`;
+	DROP TABLE IF EXISTS `{$sliderTableName}`;
+
+	CREATE TABLE `{$sliderTableName}` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`name` VARCHAR(128) NOT NULL,
+		`is_active` BOOLEAN NOT NULL DEFAULT TRUE,
+		PRIMARY KEY (`id`)
+	);
+
+	CREATE TABLE `{$imageTableName}` (
+		`id` INT NOT NULL AUTO_INCREMENT,
+		`filename` TEXT NOT NULL,
+		`slider_id` INT NOT NULL,
+		`is_active` BOOLEAN NOT NULL DEFAULT TRUE,
+		PRIMARY KEY (`id`),
+		FOREIGN KEY (`slider_id`)
+			REFERENCES `slider`(`id`)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+	);
+");
+
+$this->endSetup();
