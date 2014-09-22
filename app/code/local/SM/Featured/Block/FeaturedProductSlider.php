cc212af4b9e1d5d80da65d5a3ae085be1dbf3bc8
Nam Mai Anh <namkingkong@hotmail.com>
2014-09-22 16:00:03 +0700

    Update

--- app/code/local/SM/Featured/Block/FeaturedProductSlider.php
+++ app/code/local/SM/Featured/Block/FeaturedProductSlider.php
@@ -36,9 +36,18 @@ class SM_Featured_Block_FeaturedProductSlider extends Mage_Core_Block_Template {
 	 * @return Mage_Catalog_Model_Resource_Product_Collection
 	 */
 	protected function _getFeaturedProducts($placeIds) {
-		return Mage::getModel('catalog/product')->getCollection()
+		$products = Mage::getModel('catalog/product')->getCollection()
 			->addAttributeToFilter('is_featured', 1)
 			->addAttributeToFilter('featured_place', array('in' => $placeIds));
+
+		// Filter by category if current category differ ABSOLUTELY from null
+		if (($currentCategory = Mage::registry('current_category')) !== null) {
+			$products
+				->joinField('category_id', 'catalog/category_product', 'category_id', 'product_id = entity_id', null, 'left')
+				->addAttributeToFilter('category_id', $currentCategory->getId());
+		}
+
+		return $products;
 	}
 
 }
---
+++ skin/frontend/rwd/default/SM/js/Featured.js
@@ -0,0 +1,8 @@
+jQuery(function() {
+    var gallery = jQuery('.featured-products-swiper-container').swiper({
+        slidesPerView : 'auto',
+        resizeReInit:true,
+        keyboardControl:true,
+        grabCursor:true
+    });
+});
