428ef58131be182e84895f9f042d52a43421966c
Nam Mai Anh <namkingkong@hotmail.com>
2014-09-23 16:09:45 +0700

    New Module: SM_BestSeller

---
+++ app/code/local/SM/BestSeller/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: namma
+ * Date: 9/23/14
+ * Time: 4:06 PM
+ */
+class SM_BestSeller_Helper_Data extends Mage_Core_Helper_Abstract {
+	// Code here...
+}
