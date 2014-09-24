d0910efdead032ef24d7305ffcc6086d43dff042
Nam Mai Anh <namkingkong@hotmail.com>
2014-09-25 03:51:05 +0700

    Update

--- app/code/local/SM/BestSeller/Block/BestSeller.php
+++ app/code/local/SM/BestSeller/Block/BestSeller.php
@@ -5,31 +5,52 @@
  * Date: 9/24/14
  * Time: 3:45 PM
  */
-class SM_BestSeller_Block_BestSeller extends Mage_Core_Block_Abstract {
+class SM_BestSeller_Block_BestSeller extends Mage_Core_Block_Template {
 
     public function getBestSellerProducts() {
         $storeId = Mage::app()->getStore()->getId();
 
-        $products = Mage::getResourceModel('catalog/product_collection')
-            ->addOrdererQty()
-            ->addAttributeToSelect('*')
+        $products = Mage::getResourceModel('reports/product_collection')
+            ->addOrderedQty()
+//            ->addAttributeToSelect('*')
             ->addAttributeToSelect(array('name', 'price', 'small_image'))
             ->setStoreId($storeId)
             ->addStoreFilter($storeId)
             ->setOrder('ordered_qty', 'desc');
 
+	    if (($currentCategory = Mage::registry('current_category')) !== null) {
+		    $products
+		        ->joinField('category_id', 'catalog/category_product', 'category_id', 'product_id = entity_id')
+			    ->addAttributeToFilter('category_id', array('in', $this->_getAllSubCategories($currentCategory->getId())));
+	    }
+
         // Filter collection with product status
         Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
 
         // Filter collection with product visibility
-        Mage::getSingleton('catalog/product_visibility')->addVisibleFilterCollection($products);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($products);
 
         // Set limit quantity from system configuration
-        /*
-         * Code here...
-         */
+	    $products->setPageSize(10)->setCurPage(1);
 
         return $products;
     }
 
+	public function getCurrentCurrencySymbol() {
+		return Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol();
+	}
+
+	private function _getAllSubCategories($rootId) {
+		$tree = array($rootId);
+
+		$children = Mage::getModel('catalog/category')
+			->getCollection()
+			->addAttributeToFilter('parent_id', $rootId);
+
+		foreach ($children as $child) {
+			$tree = array_merge($tree, $this->_getAllSubCategories($child->getId()));
+		}
+
+		return $tree;
+	}
 }
--- skin/frontend/rwd/default/SM/js/Featured.js
+++
@@ -1,8 +0,0 @@
-jQuery(function() {
-    var gallery = jQuery('.featured-products-swiper-container').swiper({
-        slidesPerView : 'auto',
-        resizeReInit:true,
-        keyboardControl:true,
-        grabCursor:true
-    });
-});
